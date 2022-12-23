9adfa4658157befc622d8a4d95e0140b8a4319ce
ruslanguns <ruslanguns@gmail.com>
2022-12-23 17:32:41 +0000

    Update a REST resource

--- App.tsx
+++ App.tsx
@@ -5,13 +5,15 @@ import * as arrayMove from 'array-move';
 interface TodoItem {
   id: number;
   text: string;
+  next: number | null;
+  prev: number | null;
 }
 
 const mockedDatabase: TodoItem[] = [
-  { id: 1, text: 'Take out the trash' },
-  { id: 2, text: 'Do the dishes' },
-  { id: 3, text: 'Buy groceries' },
-  { id: 4, text: 'Do laundry' },
+  { id: 1, text: 'Take out the trash', prev: null, next: 1 },
+  { id: 2, text: 'Do the dishes', prev: 0, next: 2 },
+  { id: 3, text: 'Buy groceries', prev: 1, next: 3 },
+  { id: 4, text: 'Do laundry', prev: 2, next: null },
 ];
 
 const SortableItem = SortableElement<{ value: TodoItem }>(({ value }) => {
@@ -31,7 +33,7 @@ const SortableList = SortableContainer<{ items: TodoItem[] }>(({ items }) => {
 function TodoList() {
   const [items, setItems] = React.useState<TodoItem[]>(mockedDatabase);
 
-  const onSortEnd = ({
+  const onSortEnd = async ({
     oldIndex,
     newIndex,
   }: {
@@ -39,7 +41,52 @@ function TodoList() {
     newIndex: number;
   }) => {
     setItems(arrayMove.arrayMoveImmutable(items, oldIndex, newIndex));
-    console.log(items);
+
+    try {
+      const movedItem = items[newIndex];
+      const prevItem = items[newIndex - 1];
+      const nextItem = items[newIndex + 1];
+
+      if (prevItem) {
+        await fetch(
+          `https://71e90f0cc14abbdd8e0f172dc9423f55.m.pipedream.net?type=UPDATE_PREV_ITEM`,
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              id: prevItem.id,
+              next: movedItem.id,
+            }),
+          }
+        );
+      }
+
+      if (nextItem) {
+        await fetch(
+          `https://71e90f0cc14abbdd8e0f172dc9423f55.m.pipedream.net?type=UPDATE_NEXT_ITEM`,
+          {
+            method: 'POST',
+            body: JSON.stringify({
+              id: nextItem.id,
+              prev: movedItem.id,
+            }),
+          }
+        );
+      }
+
+      await fetch(
+        `https://71e90f0cc14abbdd8e0f172dc9423f55.m.pipedream.net?type=UPDATE_ITEM`,
+        {
+          method: 'POST',
+          body: JSON.stringify({
+            id: movedItem.id,
+            prev: prevItem ? prevItem.id : null,
+            next: nextItem ? nextItem.id : null,
+          }),
+        }
+      );
+    } catch (error) {
+      console.error(error);
+    }
   };
 
   return <SortableList items={items} onSortEnd={onSortEnd} />;
